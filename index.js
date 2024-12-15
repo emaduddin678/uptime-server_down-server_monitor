f03e74d4398cfaa26507b2d3e861b1b2a44358c5
emaduddin678 <emaduddin678.com>
2024-12-15 12:19:29 +0600

    changes server url

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require("express");
+import express from "express";
+import { server_port } from "./secret.js";
 const app = express();
-const port = 3000;
+const port = server_port;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(
+    `Example app listening on port http://localhost:${port}`
+  );
 });
--- secret.js
+++ secret.js
@@ -1,2 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+
+export const server_port = process.env.SERVER_PORT;
